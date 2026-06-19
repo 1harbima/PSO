a46f55e103a61448c57fae13f5e49c193a1f6280
Harbima Razan <harbimarazan@gmail.com>
2026-06-19 13:32:30 +0700

    added pipeline and test sim

---
+++ test-simulation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Memulai simulasi pengujian otomatis..."
+echo "Memeriksa parameter environment..."
+echo "Integrasi ke server pengujian untuk Kelompok 9..."git
+
+# Mengubah status menjadi gagal untuk simulasi error
+echo "FATAL ERROR: Ada modul yang tidak berfungsi!"
+exit 1
