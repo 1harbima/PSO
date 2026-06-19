f2cd90b52ee0bdf6a318f5c0a81c801ffca27bf5
Harbima Razan <harbimarazan@gmail.com>
2026-06-19 13:49:16 +0700

    deleted conflicting files

--- test-simulation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Memulai simulasi pengujian otomatis..."
-echo "Memeriksa parameter environment..."
-echo "Integrasi ke server pengujian untuk Kelompok 9..."git
-
-# Mengubah status menjadi gagal untuk simulasi error
-echo "FATAL ERROR: Ada modul yang tidak berfungsi!"
-exit 1
